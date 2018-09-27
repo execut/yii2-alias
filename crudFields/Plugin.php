8ab1c6ce2dc0a7ff0d55c2c3a0747ab14e9ffaed
eXeCUT <sexecut@gmail.com>
2018-09-27 17:41:14 +0300

    Alias is setted required

--- crudFields/Plugin.php
+++ crudFields/Plugin.php
@@ -16,6 +16,7 @@ class Plugin extends \execut\crudFields\Plugin
             [
                 'attribute' => 'alias',
                 'module' => 'alias',
+                'required' => true,
             ],
         ];
     }
